03999ea3eb5af14a19eeb72ab727d268bdbe05c6
wxb <superwang9527@163.com>
2021-05-18 16:21:05 +0800

    feat：初始化项目

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './pages/App/App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
