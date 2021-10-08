62c6b1202964fbfd61693623b321305ee046b354
wxb <superwang9527@163.com>
2021-10-08 16:14:14 +0800

    feat: 修改了判定点击区域的方法,鼠标样式调整完成

--- src/commponets/draw/drawRect.ts
+++ src/commponets/draw/drawRect.ts
@@ -1,97 +1,255 @@
 interface linesItem {
-    p1: controlPointsItem
-    p2: controlPointsItem
+    o: controlPointsItem
+    d: controlPointsItem
+}
+interface ILines {
+    // 上
+    topline: linesItem,
+    // 左
+    leftline: linesItem,
+    // 右
+    rightline: linesItem,
+    // 下
+    bottomline: linesItem,
 }
 interface controlPointsItem {
     x: number;
     y: number
 }
 
-// const oCoords: any = {
-//     tl: {
-//       x: ltX,
-//       y: ltY
-//     },
-//     tr: {
-//       x: rtX,
-//       y: rtY
-//     },
-//     br: {
-//       x: rbX,
-//       y: rbY
-//     },
-//     bl: {
-//       x: lbX,
-//       y: lbY
-//     }
-//   };
-//   const point = {
-//     x: x2,
-//     y: y2
-//   };
-//   function lineBox(oCoords: any) {
+// 判断鼠标样式
+const getCursorStyle = (canvasDom, index, realRotate) => {
+    let cursorStyle = '';
+    const rotate = realRotate * 180 / Math.PI;
+    const range = rotate / 45
+    switch (index) {
+        case 0:
+            if (range <= 1) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'se-resize';
+            } else if (range <= 5) {
+                cursorStyle = 's-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'nw-resize';
+            }
+            break;
+        case 1:
+            if (range <= 1) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'se-resize';
+            } else if (range <= 4) {
+                cursorStyle = 's-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'ne-resize';
+            }
+            break;
+        case 2:
+            if (range <= 1) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'se-resize';
+            } else if (range <= 7) {
+                cursorStyle = 's-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'sw-resize';
+            }
+            break;
+        case 3:
+            if (range <= 1) {
+                cursorStyle = 'se-resize';
+            } else if (range <= 2) {
+                cursorStyle = 's-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'se-resize';
+            }
+            break;
+        case 4:
+            if (range <= 1) {
+                cursorStyle = 'n-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 4) {
+                cursorStyle = 's-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'n-resize';
+            }
+            break;
+        case 5:
+            if (range <= 1) {
+                cursorStyle = 's-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'n-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 8) {
+                cursorStyle = 's-resize';
+            }
+            break;
+        case 6:
+            if (range <= 1) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 2) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'n-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 6) {
+                cursorStyle = 's-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'w-resize';
+            }
+            break;
+        case 7:
+            if (range <= 1) {
+                cursorStyle = 'e-resize';
+            } else if (range <= 2) {
+                cursorStyle = 's-resize';
+            } else if (range <= 3) {
+                cursorStyle = 'sw-resize';
+            } else if (range <= 4) {
+                cursorStyle = 'w-resize';
+            } else if (range <= 5) {
+                cursorStyle = 'nw-resize';
+            } else if (range <= 6) {
+                cursorStyle = 'n-resize';
+            } else if (range <= 7) {
+                cursorStyle = 'ne-resize';
+            } else if (range <= 8) {
+                cursorStyle = 'e-resize';
+            }
+            break;
+        case 8:
+            cursorStyle = 'move';
+            break;
+        default:
+            cursorStyle = 'default';
+            break;
+    }
+    canvasDom.style.cursor = cursorStyle;
+}
 
-//     var lines: any = {
-//       topline: {
-//         o: oCoords.tl,
-//         d: oCoords.tr
-//       },
-//       rightline: {
-//         o: oCoords.tr,
-//         d: oCoords.br
-//       },
-//       bottomline: {
-//         o: oCoords.br,
-//         d: oCoords.bl
-//       },
-//       leftline: {
-//         o: oCoords.bl,
-//         d: oCoords.tl
-//       },
-//     };
-//     return lines;
-//   };
-//   let lines = lineBox(oCoords);
-// function pointBox(point: { y: number; x: number; }, lines: any) {
+// 获取线段
+function lineBox(oCoords: any) {
 
-//     var b1, b2, a1, a2, xi,
-//         xcount = 0,
-//         iLine;
+    var lines: any = {
+        topline: {
+            o: oCoords.tl,
+            d: oCoords.tr
+        },
+        rightline: {
+            o: oCoords.tr,
+            d: oCoords.br
+        },
+        bottomline: {
+            o: oCoords.br,
+            d: oCoords.bl
+        },
+        leftline: {
+            o: oCoords.bl,
+            d: oCoords.tl
+        },
+    };
+    return lines;
+};
+// 判断是否在点击区域内，当为xcount为1时才是在点击区域
+function pointBox(point: { y: number; x: number; }, lines: any) {
 
-//     for (var lineKey in lines) {
-//         iLine = lines[lineKey];
-//         // optimisation 1: line below point. no cross
-//         if ((iLine.o.y < point.y) && (iLine.d.y < point.y)) {
-//             continue;
-//         }
-//         // optimisation 2: line above point. no cross
-//         if ((iLine.o.y >= point.y) && (iLine.d.y >= point.y)) {
-//             continue;
-//         }
-//         // optimisation 3: vertical line case
-//         if ((iLine.o.x === iLine.d.x) && (iLine.o.x >= point.x)) {
-//             xi = iLine.o.x;
-//         }
-//         // calculate the intersection point
-//         else {
-//             b1 = 0;
-//             b2 = (iLine.d.y - iLine.o.y) / (iLine.d.x - iLine.o.x);
-//             a1 = point.y - b1 * point.x;
-//             a2 = iLine.o.y - b2 * iLine.o.x;
+    var b1, b2, a1, a2, xi,
+        xcount = 0,
+        iLine;
 
-//             xi = -(a1 - a2) / (b1 - b2);
-//         }
+    for (var lineKey in lines) {
+        iLine = lines[lineKey];
+        // optimisation 1: line below point. no cross
+        if ((iLine.o.y < point.y) && (iLine.d.y < point.y)) {
+            continue;
+        }
+        // optimisation 2: line above point. no cross
+        if ((iLine.o.y >= point.y) && (iLine.d.y >= point.y)) {
+            continue;
+        }
+        // optimisation 3: vertical line case
+        if ((iLine.o.x === iLine.d.x) && (iLine.o.x >= point.x)) {
+            xi = iLine.o.x;
+        }
+        // calculate the intersection point
+        else {
+            b1 = 0;
+            b2 = (iLine.d.y - iLine.o.y) / (iLine.d.x - iLine.o.x);
+            a1 = point.y - b1 * point.x;
+            a2 = iLine.o.y - b2 * iLine.o.x;
 
-//         if (xi >= point.x) {
-//             xcount += 1;
-//         }
-//         // optimisation 4: specific for square images
-//         if (xcount === 2) {
-//             break;
-//         }
-//     }
-//     return xcount;
-// };
+            xi = -(a1 - a2) / (b1 - b2);
+        }
+
+        if (xi >= point.x) {
+            xcount += 1;
+        }
+        // optimisation 4: specific for square images
+        if (xcount === 2) {
+            break;
+        }
+    }
+
+    return xcount === 1;
+};
 
 // 绘制控制器的线段和点
 const drawControl = (ctx, controlPoints, x, y, width, height, realRotate, callback) => {
@@ -113,25 +271,26 @@ const drawControl = (ctx, controlPoints, x, y, width, height, realRotate, callba
         ctx.fillRect(item.x, item.y, 10, 10)
         ctx.restore();
     })
-    const lines: linesItem[] = []
+    let lines: any = {};
 
-    lines[0] = {
-        p1: getEndPointByRotate([x, y], [x + width / 2, y + height / 2], realRotate),
-        p2: getEndPointByRotate([x + width, y], [x + width / 2, y + height / 2], realRotate),
+    lines.topline = {
+        o: getEndPointByRotate([x, y], [x + width / 2, y + height / 2], realRotate),
+        d: getEndPointByRotate([x + width, y], [x + width / 2, y + height / 2], realRotate),
     };
-    lines[1] = {
-        p1: getEndPointByRotate([x, y], [x + width / 2, y + height / 2], realRotate),
-        p2: getEndPointByRotate([x, y + height], [x + width / 2, y + height / 2], realRotate),
+    lines.leftline = {
+        o: getEndPointByRotate([x, y], [x + width / 2, y + height / 2], realRotate),
+        d: getEndPointByRotate([x, y + height], [x + width / 2, y + height / 2], realRotate),
     };
-    lines[2] = {
-        p1: getEndPointByRotate([x + width, y], [x + width / 2, y + height / 2], realRotate),
-        p2: getEndPointByRotate([x + width, y + height], [x + width / 2, y + height / 2], realRotate),
+    lines.rightline = {
+        o: getEndPointByRotate([x + width, y], [x + width / 2, y + height / 2], realRotate),
+        d: getEndPointByRotate([x + width, y + height], [x + width / 2, y + height / 2], realRotate),
     };
-    lines[3] = {
-        p1: getEndPointByRotate([x, y + height], [x + width / 2, y + height / 2], realRotate),
-        p2: getEndPointByRotate([x + width, y + height], [x + width / 2, y + height / 2], realRotate),
+    lines.bottomline = {
+        o: getEndPointByRotate([x, y + height], [x + width / 2, y + height / 2], realRotate),
+        d: getEndPointByRotate([x + width, y + height], [x + width / 2, y + height / 2], realRotate),
     };
-    callback(lines)
+
+    callback(lines, newControlPoints)
 }
 
 // 绘制矩形
@@ -157,81 +316,42 @@ const getEndPointByRotate = (startPoint: number[], centerPoint: number[], angle:
 const useDrawRect = (ctx, canvasDom, state, type, callback) => {
     ctx.restore();
     const { rectParams, rotate } = state
-    let lines: linesItem[] = rectParams.lines
+    let lines: ILines = rectParams.lines
+    let oCoords = rectParams.oCoords
     let controlPoints: controlPointsItem[] = rectParams.controlPoints
     let { x, y, width, height } = rectParams;
-    let isRectSelected = false;
+    let isRectSelected = false;  // 是否选中
     const realRotate = rotate * 3.6 * (Math.PI / 180);
-    let selectedControl: null | number = null
+    let selectedControl: null | number = null // 点击的控制点坐标
     let isDrag = false;
 
     // 判断是否点击中图形
     const isSelected = (e) => {
         let eventX = Math.floor(e.clientX - canvasDom.getBoundingClientRect().left);
         let eventY = Math.floor(e.clientY - canvasDom.getBoundingClientRect().top);
-        let intersectionCount = 0;
-        for (let i = 0; i < lines.length; i++) {
-            let line = lines[i];
-            // 两个顶点
-            let { p1, p2 } = line;
 
-            if ((p1.y < eventY && p2.y < eventY) || (p1.y >= eventY && p2.y >= eventY)) {
-                continue;
-            } else {
-                const sx = ((eventY - p1.y) / (p2.y - p1.y)) * (p2.x - p1.x) + p1.x;
-                if (sx >= eventX) {
-                    intersectionCount++;
-                }
-            }
-        }
+        lines = lineBox(oCoords);
 
         // 是否点击了控制器
         let isSelectedControl = false
         selectedControl = null
         const newControlPoints = controlPoints.map((itemPoints) => getEndPointByRotate([itemPoints.x, itemPoints.y], [x + width / 2, y + height / 2], realRotate))
-
         newControlPoints.forEach((item, index) => {
-            const lines: linesItem[] = []
-
-            lines[0] = {
-                p1: getEndPointByRotate([item.x, item.y], [item.x + 5, item.y + 5], realRotate),
-                p2: getEndPointByRotate([item.x + 10, item.y], [item.x + 5, item.y + 5], realRotate),
-            };
-            lines[1] = {
-                p1: getEndPointByRotate([item.x, item.y], [item.x + 5, item.y + 5], realRotate),
-                p2: getEndPointByRotate([item.x, item.y + 10], [item.x + 5, item.y + 5], realRotate),
-            };
-            lines[2] = {
-                p1: getEndPointByRotate([item.x + 10, item.y], [item.x + 5, item.y + 5], realRotate),
-                p2: getEndPointByRotate([item.x + 10, item.y + 10], [item.x + 5, item.y + 5], realRotate),
-            };
-            lines[3] = {
-                p1: getEndPointByRotate([item.x, item.y + 10], [item.x + 5, item.y + 5], realRotate),
-                p2: getEndPointByRotate([item.x + 10, item.y + 10], [item.x + 5, item.y + 5], realRotate),
-            };
-            let count = 0;
-            for (let i = 0; i < lines.length; i++) {
-                let line = lines[i];
-                // 两个顶点
-                let { p1, p2 } = line;
-
-                if ((p1.y < eventY && p2.y < eventY) || (p1.y >= eventY && p2.y >= eventY)) {
-                    continue;
-                } else {
-                    const sx = ((eventY - p1.y) / (p2.y - p1.y)) * (p2.x - p1.x) + p1.x;
-                    if (sx >= eventX) {
-                        count++;
-                    }
-                }
+            const oCoords = {
+                tl: getEndPointByRotate([item.x, item.y], [item.x, item.y], realRotate),
+                tr: getEndPointByRotate([item.x + 10, item.y], [item.x, item.y], realRotate),
+                br: getEndPointByRotate([item.x + 10, item.y + 10], [item.x, item.y], realRotate),
+                bl: getEndPointByRotate([item.x, item.y + 10], [item.x, item.y], realRotate),
             }
+            const lines = lineBox(oCoords);
 
-            if (!(count % 2 === 0)) {
-                isSelectedControl = true
+            if (pointBox({ x: eventX, y: eventY }, lines)) {
+                isSelectedControl = true;
                 selectedControl = index
             }
         })
-        console.log(selectedControl)
-        return !(intersectionCount % 2 === 0) || isSelectedControl;
+
+        return pointBox({ x: eventX, y: eventY }, lines) || (isRectSelected && isSelectedControl);
     }
 
     if (type === 'rotate') {
@@ -252,7 +372,7 @@ const useDrawRect = (ctx, canvasDom, state, type, callback) => {
             isRectSelected = true;
             drawRectBox(ctx, x, y, width, height, realRotate)
             drawControl(ctx, controlPoints, x, y, width, height, realRotate, (data) => lines = data)
-            console.log(isDrag)
+
         } else {
             isRectSelected = false
             callback({ ...rectParams, isRectSelected })
@@ -265,7 +385,14 @@ const useDrawRect = (ctx, canvasDom, state, type, callback) => {
             if (isRectSelected) {
                 drawControl(ctx, controlPoints, x, y, width, height, realRotate, (data) => lines = data)
             }
-            callback({ ...rectParams, x, y, lines, controlPoints, isRectSelected })
+
+            oCoords = {
+                tl: getEndPointByRotate([x, y], [x + width / 2, y + height / 2], realRotate),
+                tr: getEndPointByRotate([x + width, y], [x + width / 2, y + height / 2], realRotate),
+                br: getEndPointByRotate([x + width, y + height], [x + width / 2, y + height / 2], realRotate),
+                bl: getEndPointByRotate([x, y + height], [x + width / 2, y + height / 2], realRotate),
+            }
+            callback({ ...rectParams, x, y, lines, controlPoints, isRectSelected, oCoords })
         }
     }
     canvasDom.onmousemove = (e) => {
@@ -285,6 +412,26 @@ const useDrawRect = (ctx, canvasDom, state, type, callback) => {
             ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
             drawRectBox(ctx, x, y, width, height, realRotate)
             drawControl(ctx, controlPoints, x, y, width, height, realRotate, (data) => lines = data)
+        }
+        let controlIndex: null | number = null
+        if (isRectSelected) {
+            let eventX = Math.floor(e.clientX - canvasDom.getBoundingClientRect().left);
+            let eventY = Math.floor(e.clientY - canvasDom.getBoundingClientRect().top);
+            const newControlPoints = controlPoints.map((itemPoints) => getEndPointByRotate([itemPoints.x, itemPoints.y], [x + width / 2, y + height / 2], realRotate))
+            newControlPoints.forEach((item, index) => {
+                const oCoords = {
+                    tl: getEndPointByRotate([item.x, item.y], [item.x, item.y], realRotate),
+                    tr: getEndPointByRotate([item.x + 10, item.y], [item.x, item.y], realRotate),
+                    br: getEndPointByRotate([item.x + 10, item.y + 10], [item.x, item.y], realRotate),
+                    bl: getEndPointByRotate([item.x, item.y + 10], [item.x, item.y], realRotate),
+                }
+                const lines = lineBox(oCoords);
+
+                if (pointBox({ x: eventX, y: eventY }, lines)) {
+                    controlIndex = index
+                }
+            })
+            getCursorStyle(canvasDom, controlIndex, realRotate)
         }
 
     }
